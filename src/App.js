a96d3bf5a9599bdc5c3f6cd98a245bb5325c0d25
yunkigeo <ylin@hellio.com>
2023-11-29 15:14:33 +0100

    Init advent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
-function App() {
+function Case({num}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-[90%] h-[90%] min-h-[100px] border border-1 border-white flex text-white rounded m-1'>
+      <h1 className='font-bold ml-3 mt-3 text-xl'>{num}</h1>
+    </div>
+  )
+}
+function App() {
+  const caseNumbers = Array.from({ length: 24 }, (_, index) => index + 1);
+  return (
+    <div className="bg-main grid md:grid-cols-2 lg:grid-cols-3 min-h-[100vh] p-[50px]">
+      <div className=''>
+        <h1 className='text-2xl font-bold text-white'>Calendrier de l'avent</h1>
+        
+      </div>
+      <div className='lg:col-span-2 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5'>
+
+      {caseNumbers.map((num) => (
+        <Case key={num} num={num} />
+      ))}
+
+
+      </div>
+      
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'main': '#CB5354',
+      'white': "white",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
